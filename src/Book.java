153ba12fc69ba88b264d7da85af7ca803df43981
hzjanuary <nhhoang03012004@gmail.com>
2026-02-04 08:20:30 +0700

    first commit

---
+++ src/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private final int id;
+    private final String title;
+    private final String author;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Title: " + title +
+                " | Author: " + author;
+    }
+}
